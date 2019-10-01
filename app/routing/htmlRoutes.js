ecbd180b47827cf4240b3a402aa06e8ff2351a63
rb2001 <rbaird2001@yahoo.com>
2019-09-30 18:49:34 -0600

    file structure only

---
+++ app/routing/htmlRoutes.js
@@ -0,0 +1,14 @@
+const express = require("express");
+const app = express();
+
+
+//req will hold an object with all the detail about the request
+//res will hold an object with all the detail about the resonse
+//default route
+app.get("/",function(req,res){
+
+});
+//survey route
+
+//setup the listener
+app.listen(3000,function(){});
