697ac265a6385aa7a3b43b8397a1dd7cb8d89846
rb2001 <rbaird2001@yahoo.com>
2019-10-03 20:58:48 -0600

    safety commit

--- server.js
+++ server.js
@@ -2,4 +2,18 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-console.log(app);
+//console.log(app);
+
+var PORT = process.env.PORT || 8080
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+
+require("./app/routing/apiRoutes.js")(app);
+require("./app/routing/htmlRoutes.js")(app);
+
+app.listen(PORT,function(){
+    console.log(`Web App is listening on PORT: ${PORT}`);
+})
+
+
